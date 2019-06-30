37648fad3ce7d266bf90bc8c74962e314aebcdde
ACken2 <acken2@outlook.com>
2019-06-30 16:36:52 +0800

    Implemented the CerttifyDAOToken contract

---
+++ contracts/CerttifyDAOToken.sol
@@ -0,0 +1,55 @@
+pragma solidity 0.5.3;
+
+import '../node_modules/openzeppelin-solidity/contracts/token/ERC777/ERC777.sol';
+import '../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol';
+import '../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol';
+
+/**
+ * @title CerttifyDAO Token Contract
+ *
+ * @author Ken Sze <acken2@outlook.com>
+ *
+ * @notice This contracts defines a token that grants owners a right to participate in the decentralized governance
+ * of Certtify protocol - CerttifyDAO.
+ *
+ * @dev This token contract obeys the ERC-777 standard while being backward compatible to ERC-20 standard.
+ */
+contract CerttifyDAOToken is ERC777, Ownable {
+
+    // Use safe math
+    using SafeMath for uint256;
+
+    // Define token specification according to ERC-777
+    string constant TOKEN_NAME = "CerttifyDAO Token";
+    string constant SYMBOL = "CDT";
+    address[] private DEFAULT_OPERATORS = new address[](0);
+
+    // Initial token supply is 10,000,000 CDT
+    uint256 private INITIAL_SUPPLY = uint256(10000000).mul(uint256(10**18));
+
+    /**
+     * @notice Initialize the CerttifyDAOToken contract
+     * @param wallet The address that would receive the initial minted token
+     * @param DAO The address of the CerttifyDAO
+     */
+    constructor(address wallet, address DAO) ERC777(TOKEN_NAME, SYMBOL, DEFAULT_OPERATORS) public {
+        // Check input
+        require(wallet != address(0), "CDT: initial token send to the zero address");
+        require(DAO != address(0), "CDT: DAO address cannot be the zero address");
+        // Mint initial token supply
+        _mint(msg.sender, wallet, INITIAL_SUPPLY, "", "");
+        // Transfer ownership to CerttifyDAO
+        transferOwnership(DAO);
+    }
+
+    /**
+     * @notice Mint interest for recipient to allow CerttifyDAO to observe the Certtify protocol
+     * @param recipient The address that would receive the minted interest
+     * @param amount The amount of interest to be minted
+     */
+    function mintInterest(address recipient, uint256 amount) external onlyOwner() {
+        // Mint interest for recipient
+        _mint(msg.sender, recipient, amount, "", "");
+    }
+
+}
