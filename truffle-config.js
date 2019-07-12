ba6ba1bd1b138f2ea78727929f341d8ec058d18a
ACken2 <acken2@outlook.com>
2019-07-12 18:17:06 +0800

    Fixed out of gas error during coverage run

--- truffle-config.js
+++ truffle-config.js
@@ -9,7 +9,8 @@ module.exports = {
         coverage: {
             host: '127.0.0.1',
             port: 8555,
-            network_id: "*"
+            network_id: "*",
+            gas: 0xfffffffffff
         }
     },
 
